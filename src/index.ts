60638557133f6a56252fa8133104bfdd8845b09e
Devansh Chouhan <devanshchouhan7112003@gmail.com>
2026-04-17 13:56:54 +0530

    Added PORT using dotenv

--- src/index.ts
+++ src/index.ts
@@ -223,8 +223,10 @@ async function main() {
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log(err));
 
-  app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
 }
 main();
